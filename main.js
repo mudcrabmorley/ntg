b29ebdebe234bb27cd4cd2c16df921faeaec88c4
Jake Morley <jakemorley@Jakes-MacBook-Pro.local>
2024-04-19 22:08:30 +1000

    final touches

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-// HEAD/FOOTER IMPORT
-window.onload = function () {
-  loadFooter();
-};
-
-function loadFooter() {
-  fetch("footer.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("footer").innerHTML = data;
-    });
-}
-
 // PAGE DISPLAY AFTER LOAD
 window.onload = function () {
   document.body.style.display = "block";
