6fba267ee2cdf5c9fbc493e9c63c1eec56f9a3c4
Jake Morley <jakemorley@Jakes-MacBook-Pro.local>
2024-04-19 15:39:01 +1000

    seo fix up

--- main.js
+++ main.js
@@ -1,16 +1,7 @@
 // HEAD/FOOTER IMPORT
 window.onload = function () {
-  loadHeader();
   loadFooter();
 };
-
-function loadHeader() {
-  fetch("head.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("head").innerHTML = data;
-    });
-}
 
 function loadFooter() {
   fetch("footer.html")
